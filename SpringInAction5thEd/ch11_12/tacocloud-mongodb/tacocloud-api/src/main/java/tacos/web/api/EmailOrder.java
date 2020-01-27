ce0f62453e0ffe395b95b520cb2725527dee3cb8
Matthias Häußer <Matthias.Haeusser@EXXETA.com>
2020-01-27 18:08:40 +0100

    Add chapter 11_12

---
+++ SpringInAction5thEd/ch11_12/tacocloud-mongodb/tacocloud-api/src/main/java/tacos/web/api/EmailOrder.java
@@ -0,0 +1,18 @@
+package tacos.web.api;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class EmailOrder {
+
+    private String email;
+    private List<EmailTaco> tacos;
+    
+    @Data
+    public static class EmailTaco {
+        private String name;
+        private List<String> ingredients;
+    }    
+}
