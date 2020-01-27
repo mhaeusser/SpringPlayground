9f956b6220711998282b584a9c70895dbb80ec01
Matthias Häußer <Matthias.Haeusser@EXXETA.com>
2020-01-27 18:20:09 +0100

    Add chapter 16

---
+++ SpringInAction5thEd/ch16/taco-service/src/main/java/tacos/tacos/TacoRepository.java
@@ -0,0 +1,7 @@
+package tacos.tacos;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface TacoRepository 
+                 extends PagingAndSortingRepository<Taco, Long> {
+}
